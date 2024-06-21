506f2a56526e9c4a92a3fcd91496662a64aa950d
IsraelOho <151618401+ohoisrael@users.noreply.github.com>
2024-06-21 10:27:24 +0000

    Update Payments.jsx

--- frontend/src/components/Payments.jsx
+++ frontend/src/components/Payments.jsx
@@ -21,20 +21,18 @@ function Payments() {
   }, [currentUser]);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="overflow-x-auto">
       <h2 className="text-2xl font-semibold mb-4">Payments</h2>
       <Table>
         <TableHead>
-            <TableHeadCell>Username</Table.HeadCell>
-            <TableHeadCell>Amount</Table.HeadCell>
-            <TableHeadCell>Payment Date</Table.HeadCell>
+            <TableHeadCell>Username</TableHeadCell>
+            <TableHeadCell>Amount</TableHeadCell>
+            <TableHeadCell>Payment Date</TableHeadCell>
           </TableHead>     
         <TableBody className="divide-y">
           {payments.map((payment) => (
             <TableRow key={payment._id} className="text-center">
               <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{payment.userId && payment.userId.username ? payment.userId.username : currentUser.username || 'Unknown'}</TableCell>
-
-
               <TableCell className="px-4 py-2 border-b">{payment.amount}</TableCell>
               <TableCell className="px-4 py-2 border-b">{new Date(payment.paymentDate).toLocaleDateString()}</TableCell>
               </TableRow>
